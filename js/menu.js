e6c83afaa0b09769ee733376b876be2ef4ce57d4
tiago-progwebt23 <tiago.progwebt23b@gmail.com>
2023-10-19 16:32:41 -0300

    JS inicial do menu

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Selecionando o elemento (através de descendência) que acionará o menu
+const botaoMenu = document.querySelector("nav h2");
+
+// Selecionando a lista/menu através da classe
+const menu = document.querySelector(".menu");
+
+// Selecionando o link que está dentro do nav h2
+const textoBotao = botaoMenu.querySelector("a");
+
+
+
